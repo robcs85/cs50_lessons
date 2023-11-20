81d57eabf38b138dfac195e2cfd1ddbb172ab0f4
robcs85 <144968647+robcs85@users.noreply.github.com>
2023-11-20 17:03:39 -0400

    Django project

---
+++ Python/python_django/hello/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+"""
+# Create your views here.
+# funcao inde pega um argumento request que é acessada pelo user, retorna para a página através do argumento passado na função
+# uma mensagem.
+-------------------------------------------
+HttpResponse pertence a django, para usa-lo aqui deve ser importada
+Com a importação da função HttpResponse, foi passado uma ação para que o request da função index retorne a mensagem na tela
+"""
+
+
+def index(request):
+    return HttpResponse("Olá, Roberson, bem vindo ao mundo Django")
+def rob(request):
+    return HttpResponse("Olá, Roberson")
+def bob(request):
+    return HttpResponse("Ola, Bob")
+# Definindo uma função GREET, quando requisitada retornará um nome qualquer digitado pelo usuario, sem precisar
+# definir uma função para cada nome como foi feito acima, ao chamar hello/fulano, trasmitirá olá, fulano mesmo que
+# não tenha essa função definida. Agora em urls.py defino o caminho de busca
+def greet(request, nome):
+    return HttpResponse(f"Olá, {nome.capitalize()}!") #captalize formada primeira letra em maiúscula
