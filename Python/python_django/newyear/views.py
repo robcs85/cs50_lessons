e6a1ed8da1919277f420461696c1b4c6184f1b6f
robcs85 <144968647+robcs85@users.noreply.github.com>
2023-11-21 22:37:47 -0400

    New year app

---
+++ Python/python_django/newyear/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+"""
+    Definindo a função index que irá fazer uma verificação booleana se é primeiro de janeiro de 2024 ou não
+"""
+
+def index(request):
